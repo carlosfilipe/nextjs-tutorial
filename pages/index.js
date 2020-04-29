6a603c36cbec41b3b78406b19ab04d6ace301efb
Carlos Filipe <carlosfilipe.lb@gmail.com>
2020-04-29 12:08:15 -0300

    dynamic rendering

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
+import Date from '../components/date';
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,6 +21,39 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.list}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`} >
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+             
+            </li>
+          ))}
+
+        </ul>
+
+      </section>
+
     </Layout>
   )
 }
+
+export async function getStaticProps() {
+  console.log("getStaticProps");
+  const allPostsData = getSortedPostsData();
+  console.log(allPostsData)
+  return {
+    props: {
+      allPostsData
+    }
+  };
+};
